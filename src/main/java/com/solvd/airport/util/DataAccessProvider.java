64ef28549724234fc7539bd8d83fbef2b051f2f9
dctii <danactolman@gmail.com>
2024-01-09 11:44:47 -0800

    renames for JDBCImpl applied during refactor

--- src/main/java/com/solvd/airport/util/DataAccessProvider.java
+++ src/main/java/com/solvd/airport/util/DataAccessProvider.java
@@ -18,81 +18,81 @@ public class DataAccessProvider {
     }
 
     public static AddressDAO getAddressDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AddressMyBatisImpl() : new AddressDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AddressMyBatisImpl() : new AddressJDBCImpl();
     }
 
     public static AirlineDAO getAirlineDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AirlineMyBatisImpl() : new AirlineDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AirlineMyBatisImpl() : new AirlineJDBCImpl();
     }
     public static AirportDAO getAirportDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AirportMyBatisImpl() : new AirportDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AirportMyBatisImpl() : new AirportJDBCImpl();
     }
 
     public static AirlineStaffMemberDAO getAirlineStaffMemberDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AirlineStaffMemberMyBatisImpl() : new AirlineStaffMemberDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new AirlineStaffMemberMyBatisImpl() : new AirlineStaffMemberJDBCImpl();
     }
 
     public static BaggageDAO getBaggageDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new BaggageMyBatisImpl() : new BaggageDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new BaggageMyBatisImpl() : new BaggageJDBCImpl();
     }
 
     public static BoardingPassDAO getBoardingPassDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new BoardingPassMyBatisImpl() : new BoardingPassDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new BoardingPassMyBatisImpl() : new BoardingPassJDBCImpl();
     }
 
     public static BookingDAO getBookingDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new BookingMyBatisImpl() : new BookingDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new BookingMyBatisImpl() : new BookingJDBCImpl();
     }
 
     public static CheckInDAO getCheckInDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new CheckInMyBatisImpl() : new CheckInDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new CheckInMyBatisImpl() : new CheckInJDBCImpl();
     }
 
     public static EmailAddressDAO getEmailAddressDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new EmailAddressMyBatisImpl() : new EmailAddressDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new EmailAddressMyBatisImpl() : new EmailAddressJDBCImpl();
     }
 
     public static FlightDAO getFlightDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new FlightMyBatisImpl() : new FlightDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new FlightMyBatisImpl() : new FlightJDBCImpl();
     }
 
     public static GateDAO getGateDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new GateMyBatisImpl() : new GateDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new GateMyBatisImpl() : new GateJDBCImpl();
     }
 
     public static PassportDAO getPassportDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PassportMyBatisImpl() : new PassportDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PassportMyBatisImpl() : new PassportJDBCImpl();
     }
 
     public static CountryDAO getCountryDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new CountryMyBatisImpl() : new CountryDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new CountryMyBatisImpl() : new CountryJDBCImpl();
     }
 
     public static TerminalDAO getTerminalDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new TerminalMyBatisImpl() : new TerminalDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new TerminalMyBatisImpl() : new TerminalJDBCImpl();
     }
 
     public static TimezoneDAO getTimezoneDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new TimezoneMyBatisImpl() : new TimezoneDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new TimezoneMyBatisImpl() : new TimezoneJDBCImpl();
     }
 
     public static PersonAddressDAO getPersonAddressDao() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonAddressMyBatisImpl() : new PersonAddressDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonAddressMyBatisImpl() : new PersonAddressJDBCImpl();
     }
 
     public static PersonEmailAddressDAO getPersonEmailAddressDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonEmailAddressMyBatisImpl() : new PersonEmailAddressDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonEmailAddressMyBatisImpl() : new PersonEmailAddressJDBCImpl();
     }
 
     public static PersonInfoDAO getPersonInfoDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonInfoMyBatisImpl() : new PersonInfoDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonInfoMyBatisImpl() : new PersonInfoJDBCImpl();
     }
 
     public static PersonPhoneNumberDAO getPersonPhoneNumberDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonPhoneNumberMyBatisImpl() : new PersonPhoneNumberDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PersonPhoneNumberMyBatisImpl() : new PersonPhoneNumberJDBCImpl();
     }
 
     public static PhoneNumberDAO getPhoneNumberDAO() {
-        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PhoneNumberMyBatisImpl() : new PhoneNumberDAOImpl();
+        return ConfigConstants.DATABASE_IMPLEMENTATION_VAL_MYBATIS.equals(databaseImpl) ? new PhoneNumberMyBatisImpl() : new PhoneNumberJDBCImpl();
     }
 }
