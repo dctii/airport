ab2051008662dd13c147be5caf51b4bb7e67c6c9
dctii <danactolman@gmail.com>
2024-02-01 23:38:19 -0800

    delete unused files

--- src/test/java/com/solvd/airport/ExampleTest.java
+++
@@ -1,6 +0,0 @@
-package com.solvd.airport;
-public class ExampleTest {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/test/java/com/solvd/airport/util/TestDataConstants.java
+++
@@ -1,5 +0,0 @@
-package com.solvd.airport.util;
-
-public class TestConstants {
-
-}
